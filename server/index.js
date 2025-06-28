d8cda1c7aa2583a203552dbfed8d724c81019134
sahil04sharma <interactsahil5232@gmail.com>
2025-06-28 12:17:20 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -89,6 +89,8 @@ app.post('/submit', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
